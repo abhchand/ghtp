133cd7bd38092861deddf2f5f3c1fd119ec6e515
Abhishek <abhishek@callrail.com>
2019-06-22 18:41:39 -0400

    Refactoring querying logic into `pull_request_service.go`

--- sync.go
+++ sync.go
@@ -1,14 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"regexp"
-	"time"
 )
 
 var flagsForSync = defineFlagsForSync()
@@ -49,127 +43,11 @@ func defineFlagsForSync() flag.FlagSet {
 
 }
 
-func issuesEndpoint() string {
-
-	return fmt.Sprintf(
-		"https://api.github.com/repos/%s/%s/issues?state=open&direction=desc",
-		githubOrganization,
-		githubRepository)
-
-}
-
-func buildRequest(url string) *http.Request {
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	req.Header.Set("User-Agent", "abhchand/ghtp")
-	req.Header.Set("Authorization", "token "+githubAuthToken)
-
-	return req
-
-}
-
-func parseNextUrl(linkHeader []string) string {
-
-	// Github responsds with a link header that contains meta information
-	// about the next page and the last page
-	//
-	//     <https://api.github.com/resource?page=2>; rel="next", \n
-	//       <https://api.github.com/resource?page=5>; rel="last"
-	//
-	// 1. Link header may not always be present. If there's not one, there is
-	//    no next page so return nil
-	//
-	// 2. Github conveniently returns the full URL required to query the next
-	//    page, including any query params you originally included. So we
-	//    extract that URL if it exists and rely on it blindly.
-	//
-	// 3. Golang's http returns the link header as a slice of strings. Not sure
-	//    if it would ever have more than one element, but we only care about
-	//    the first element/string
-
-	if len(linkHeader) == 0 {
-		return ""
-	}
-
-	re := regexp.MustCompile(`(http[^,\s]*)\>; rel=\"next\"`)
-	matches := re.FindAllStringSubmatch(linkHeader[0], 1)
-
-	if len(matches) > 0 {
-		return matches[0][1]
-	}
-
-	return ""
-
-}
-
 func runSync(cmd *Command, args []string) {
 
 	validateOptions()
 
-	httpClient := http.Client{Timeout: time.Second * 2}
-
-	var prList PullRequestList
-	url := issuesEndpoint()
-
-	for {
-
-		// Build request
-		request := buildRequest(url)
-		log.Info("Querying: " + url)
-
-		// Query API
-		response, err := httpClient.Do(request)
-		if err != nil {
-			log.Fatal(err)
-			panic(err)
-		}
-
-		// Handle bad HTTP response
-		log.Debugf("Response Status: %s", response.Status)
-		if response.StatusCode < 200 || response.StatusCode > 299 {
-			log.Fatal("Error querying API. Exiting")
-			os.Exit(1)
-		}
-
-		// Parse response
-		body, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			log.Fatal(err)
-			panic(err)
-		}
-
-		// Load Response
-		var prs PullRequestList
-		err = json.Unmarshal([]byte(body), &prs)
-		if err != nil {
-			log.Fatal(err)
-			panic(err)
-		}
-
-		// Consider only the Pull Requests marked for sync
-		for _, pr := range prs {
-			if pr.ShouldSync() {
-				prList = append(prList, pr)
-			}
-		}
-
-		// Check if we should continue
-		nextUrl := parseNextUrl(response.Header["Link"])
-		log.Debugf("Next Page: %s", nextUrl)
-
-		if len(nextUrl) > 0 {
-			url = nextUrl
-		} else {
-			break
-		}
-
-	}
-
-	fmt.Println(prList)
+	prs := findAllPullRequests()
+	fmt.Println(prs)
 
 }
