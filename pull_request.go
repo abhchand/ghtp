ced78b7fb09a957fb7aa83e677b67c7841b75d8d
Abhishek <abhishek@callrail.com>
2019-06-23 01:33:24 -0400

    Capture `pull_request` key to determine if the issue is truly a pull_request

--- pull_request.go
+++ pull_request.go
@@ -9,23 +9,40 @@ type PullRequest struct {
 	HtmlUrl string `json:"html_url"`
 	Title   string `json:"title"`
 
-	Labels  []PullRequestLabel `json:"labels"`
+	Labels          []PullRequestLabel  `json:"labels"`
+	PullRequestData map[string]string `json:"pull_request"`
 }
 
 type PullRequestList []PullRequest
 
+// NOTE: Github API treats all pull requests as Issues. That is, all pull
+// requests are issues but not all issues are pull requests.
+//
+// In fact, the underlying API endpoint we are querying is for issues, but
+// we are modeling this data structure as a Pull Request for convenience
+// sake.
+//
+// To dinstiguish which issues are in fact truly pull requests, Github
+// recommends looking for the presence of a `pull_request` key, which
+// is what this function looks for
+func (pr *PullRequest) IsTrulyPullRequest() bool {
+
+	return len(pr.PullRequestData) > 0
+
+}
+
 // Check whether this Pull Request is eligible to be included
 // in the sync process by looking at the following conditions:
 //
+//   - If PR is truly a Pull Request and not an Issue (see
+//     `IsTrulyPullRequest() function)
 //   - If PR Title starts with '[TP#1234]', case insensitive.
 //
-// NOTE: The Github API returns a list of "issues", and all pull requests
-// are issues (although not all issues are pull requests). Given the limited
-// number of fields we parse here, we still blindly model all the responses
-// as pull requests anyway. And if we retrieve an issue that matches the
-// above conditions, we process it anyway.
-//
 func (pr *PullRequest) ShouldSync() bool {
+
+	if !pr.IsTrulyPullRequest() {
+		return false
+	}
 
 	re := regexp.MustCompile(`^(\[TP#\d+\])`)
 	matches := re.FindAllStringSubmatch(pr.Title, 1)
